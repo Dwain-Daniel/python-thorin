7436780dc1a058afe889d13ad2b6327e4a526ca7
Dwain-Daniel <dwain_90@hotmail.co.uk>
2021-03-23 23:55:45 +0000

    Created additional pages

--- run.py
+++ run.py
@@ -10,25 +10,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+app.route("/")
 def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+app.route("/")
 def contact():
     return render_template("contact.html")
-
-
-@app.route("/careers")
-def careers():
-    return render_template("careers.html")
-
 
 
 if __name__ =="__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT","5000")),
-        debug=True
-    )
+        debug=True)
